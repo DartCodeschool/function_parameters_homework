13ad5974d1fef7497af4805276b7fac8bd1c2f70
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-31 14:38:58 +0500

    asdf

--- test/function_parameters_homework_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:function_parameters_homework/function_parameters_homework.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
